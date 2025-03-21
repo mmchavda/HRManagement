9b090fd7c497cc086f52b69b8e59a4cfd92b8c41
Monika Verma <monikakasanya@gmail.com>
2025-03-21 15:30:14 +0530

    Create Default admin and employee user for intitial Use

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+# Create a default admin user
+# Create or find the admin user
+  user = User.find_or_create_by!(email: "test@gmail.com", role: "admin") do |user|
+    user.password = "password123"
+    user.password_confirmation = "password123"
+    user.username = "admin"
+  end
+  
+  # Create or find the employee user
+  employee = User.find_or_create_by!(email: "test.employee@gmail.com", role: "employee") do |user|
+    user.password = "password123"
+    user.password_confirmation = "password123"
+    user.username = "test"
+  end
