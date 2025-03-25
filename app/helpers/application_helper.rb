f0369710dcc227b47c282c0b8529ab8b28051eb2
shivani shilpi <shivani.shilpi@techcronus.com>
2025-03-25 17:21:55 +0530

    Updated UI

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+	def nav_link(name, path)
+		class_name = current_page?(path) ? 'bg-gray-700 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'
+		link_to name, path, class: "px-3 py-2 rounded-md text-sm font-medium #{class_name}"
+	end
 end
