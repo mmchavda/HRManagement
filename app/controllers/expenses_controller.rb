d65cc2ad5bb4b13f543dae9186074c93bef85206
Monika Verma <monikakasanya@gmail.com>
2025-03-25 18:16:37 +0530

    User management

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -44,21 +44,15 @@ class ExpensesController < ApplicationController
     end
 
     def export_csv
-      # Get the reimbursement_requests you want to export, for example, all expenses
       @expense = Expense.all
-  
-      # Create the CSV data
       csv_data = CSV.generate(headers: true) do |csv|
-        # Add the headers (column names)
         csv << ['ID', 'User', 'Amount', 'Description', 'Date', 'Category', 'Created At', 'Updated At']
         
-        # Add expense data
         @expense.each do |expense|
           csv << [expense.id, expense.user.email, expense.amount, expense.description, expense.date,expense.category, expense.created_at, expense.updated_at]
         end
       end
   
-      # Send the CSV as a download
       send_data csv_data, filename: "expenses_#{Date.today}.csv", type: 'text/csv', disposition: 'attachment'
     end
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,42 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :destroy, :show]
+
+  # Display all users (only for admins and hr)
+  def index
+    @users = User.all
+  end
+
+  # Edit a user
+  def edit
+  end
+
+	def show 
+	end 
+
+  # Update a user
+  def update
+    if @user.update(user_params)
+      redirect_to users_path, notice: 'User was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # Destroy a user
+  def destroy
+    @user.destroy
+    redirect_to users_path, notice: 'User was successfully deleted.'
+  end
+
+  private
+
+  # Set the user based on ID
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Strong parameters for user
+  def user_params
+    params.require(:user).permit(:email, :role)
+  end
 end
