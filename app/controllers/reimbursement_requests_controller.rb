a3378062f3423c1801b2d900fbedf43cf0ecd804
Monika Verma <monikakasanya@gmail.com>
2025-04-10 13:28:23 +0530

    added rejection reason

--- app/controllers/reimbursement_requests_controller.rb
+++ app/controllers/reimbursement_requests_controller.rb
@@ -68,14 +68,36 @@ class ReimbursementRequestsController < ApplicationController
       end
     end
 
+    # def reject_request
+    #   begin
+    #     if @reimbursement_request.status == 'rejected'
+    #       redirect_to reimbursement_requests_url, alert: 'This request is already rejected.'
+    #     else
+    #       @reimbursement_request.reject!
+    #       redirect_to reimbursement_requests_url, notice: 'Reimbursement request was successfully rejected.'
+    #     end
+    #   rescue => e
+    #     redirect_to reimbursement_requests_url, alert: "Error rejecting reimbursement request: #{e.message}"
+    #   end
+    # end
+
     def reject_request
       begin
-        if @reimbursement_request.status == 'rejected'
-          redirect_to reimbursement_requests_url, alert: 'This request is already rejected.'
-        else
-          @reimbursement_request.reject!
-          redirect_to reimbursement_requests_url, notice: 'Reimbursement request was successfully rejected.'
+        reason = params[:reason]
+      
+        if reason.blank?
+          flash[:alert] = "Rejection reason is required."
+          redirect_back fallback_location: reimbursement_requests_path
+          return
         end
+      
+        if @reimbursement_request.update(status: :rejected, rejection_reason: reason)
+          flash[:notice] = "Reimbursement request rejected successfully."
+          redirect_to reimbursement_requests_path
+        else 
+          flash[:alert] = @reimbursement_request.errors.full_messages.to_sentence
+          redirect_back fallback_location: reimbursement_requests_path
+        end  
       rescue => e
         redirect_to reimbursement_requests_url, alert: "Error rejecting reimbursement request: #{e.message}"
       end
