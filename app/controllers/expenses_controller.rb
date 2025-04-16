14090cd13de24e9fe5d35dfabc424a19d522b840
Monika Verma <monikakasanya@gmail.com>
2025-04-16 19:11:13 +0530

    Added the search functionality

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -53,7 +53,6 @@ class ExpensesController < ApplicationController
       if params[:start_date].present? && params[:end_date].present?
         start_date = Date.parse(params[:start_date])
         end_date = Date.parse(params[:end_date])
-        # Replace `Ticket` with the appropriate model per report
         @expense = Expense.where(created_at: start_date.beginning_of_day..end_date.end_of_day)
       else
         @expense = Expense.all
