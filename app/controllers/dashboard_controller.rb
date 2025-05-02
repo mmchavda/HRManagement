957ea73eb2d6fcf6046f6912db7732f1c67e21b7
Monika Verma <monikakasanya@gmail.com>
2025-05-02 17:55:45 +0530

    Work with assets dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -9,6 +9,8 @@ class DashboardController < ApplicationController
       @expenses = Expense.all
       @tickets = Ticket.all
       @reimbursements = ReimbursementRequest.all
+      @recent_assets = Asset.order(created_at: :desc).limit(5)
+      @assets = Asset.all
     elsif current_user.employee?
       @recent_expenses = current_user.expenses.order(created_at: :desc).limit(5)
       @recent_tickets = current_user.created_tickets.order(created_at: :desc).limit(5)
@@ -16,6 +18,10 @@ class DashboardController < ApplicationController
       @expenses = current_user.expenses
       @reimbursements = current_user.reimbursement_requests
       @tickets = current_user.created_tickets
+      @assets = Asset.joins(:asset_assignments)
+               .where(asset_assignments: { user_id: current_user.id, active: true })
+
+      @recent_assets = @assets.order(created_at: :desc).limit(5)
     end   
   end
 
