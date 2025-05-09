9eaf8a452662f05a002067b589f27517e04c0f55
Monika Verma <monikakasanya@gmail.com>
2025-05-09 15:58:25 +0530

    2.serach:- When user is remove the serach text then list is not shown. added live search functionality

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,9 +3,11 @@ import HelloController from "controllers/hello_controller"
 import ConfirmModalController from "controllers/confirm_modal_controller"
 import AuditToggleController from "controllers/audit_toggle_controller"
 import ToggleController from "controllers/toggle_controller"
+import LiveSearchController from "controllers/live_search_controller"
 
 const application = Application.start()
 application.register("confirm-modal", ConfirmModalController)
 application.register("audit-toggle", AuditToggleController)
 application.register("hello", HelloController)
 application.register("toggle", ToggleController)
+application.register("live-search", LiveSearchController)
